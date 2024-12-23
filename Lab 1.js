b44dfdf677b9456b597d59c6a4c6410e1725e85b
MikhailoSafronov <mishasafronov1@gmail.com>
2024-12-23 20:07:48 +0200

    Lab1: додано логіку перебору та асинхронності

--- Lab 1.js
+++ Lab 1.js
@@ -1,5 +1,31 @@
 function asyncMap(array, asyncFn, doneCallback) {
-    doneCallback(null, []);
+  let results = new Array(array.length);
+  let pending = array.length;
+  let hasError = false;
+
+  if (pending === 0) {
+    return doneCallback(null, []);
   }
-  
-  
+
+  array.forEach((item, index) => {
+    asyncFn(item, (err, transformed) => {
+      if (hasError) return;
+      if (err) {
+        hasError = true;
+        return doneCallback(err);
+      }
+      results[index] = transformed;
+      pending--;
+      if (pending === 0 && !hasError) {
+        doneCallback(null, results);
+      }
+    });
+  });
+}
+// Тест:
+asyncMap([1,2,3], (num, cb) => {
+  setTimeout(() => cb(null, num*2), 500);
+}, (err, res) => {
+  console.log( err, res); // Очікуємо: null [2,4,6]
+});
+

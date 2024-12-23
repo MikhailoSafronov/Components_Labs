298abe484127e908939df1f5537b94b666081b35
MikhailoSafronov <mishasafronov1@gmail.com>
2024-12-23 21:40:23 +0200

    Lab4: Фільтрація даних під час читання

--- Lab 4.js
+++ Lab 4.js
@@ -1,13 +1,14 @@
 async function* largeDataSource() {
-    for (let i = 0; i < 3; i++) {
-        await new Promise(resolve => setTimeout(resolve, 50));
+    for (let i = 0; i < 5; i++) {
+        await new Promise(resolve => setTimeout(resolve, 30));
         yield i;
     }
 }
 
 (async () => {
     for await (const item of largeDataSource()) {
-        const transformed = item * 2;
-        console.log("Step 4.2 transformed:", transformed);
+        if (item % 2 === 0) {
+            console.log( item);
+        }
     }
 })();

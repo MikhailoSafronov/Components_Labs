4d5e57b7d30c27ef36369c6de150467792b81033
MikhailoSafronov <mishasafronov1@gmail.com>
2024-12-23 20:39:31 +0200

    Lab2:Перевірка помилок у promiseMap

--- Lab 2.js
+++ Lab 2.js
@@ -2,6 +2,15 @@ function promiseMap(array, asyncFn) {
     return Promise.all(array.map(asyncFn));
 }
 
-function doubleAsync(num) {
-    return new Promise(resolve => setTimeout(() => resolve(num*2), 500));
+function sometimesFail(num) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (num === 2) reject(new Error("Failed on 2"));
+            else resolve(num*2);
+        }, 300);
+    });
 }
+
+promiseMap([1,2,3], sometimesFail)
+    .then(res => console.log( res))
+    .catch(err => console.error("Error:", err.message)); // "Failed on 2"

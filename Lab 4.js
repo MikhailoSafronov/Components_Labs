dda056ad21178f20cf261c001c16d092f15f1090
MikhailoSafronov <mishasafronov1@gmail.com>
2024-12-23 21:38:50 +0200

    Lab4: Обробка даних одразу при отриманні

--- Lab 4.js
+++ Lab 4.js
@@ -1,12 +1,13 @@
 async function* largeDataSource() {
     for (let i = 0; i < 3; i++) {
-        await new Promise(resolve => setTimeout(resolve, 100));
+        await new Promise(resolve => setTimeout(resolve, 50));
         yield i;
     }
 }
 
 (async () => {
-    for await (const val of largeDataSource()) {
-        console.log( val);
+    for await (const item of largeDataSource()) {
+        const transformed = item * 2;
+        console.log("Step 4.2 transformed:", transformed);
     }
 })();

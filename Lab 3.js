484883a622bb84c47ee5bc7210a6b69f2c419b58
MikhailoSafronov <mishasafronov1@gmail.com>
2024-12-23 21:07:48 +0200

    Lab3: Перевірка сигналу перед кожною операцією

--- Lab 3.js
+++ Lab 3.js
@@ -1,3 +1,4 @@
+
 class FakeAbortSignal {
     constructor() {
         this.aborted = false;
@@ -18,12 +19,27 @@ function promiseMapCancelable(array, asyncFn, { signal } = {}) {
     if (signal && signal.aborted) {
         return Promise.reject(new Error("Aborted before start"));
     }
-    return Promise.all(array.map(asyncFn));
+
+    const promises = array.map(item => {
+        if (signal && signal.aborted) return Promise.reject(new Error("Aborted"));
+        return asyncFn(item, signal);
+    });
+
+    return Promise.all(promises);
+}
+
+function delayedDouble(num, signal) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (signal && signal.aborted) return reject(new Error("Aborted"));
+            resolve(num*2);
+        }, 100);
+    });
 }
 
 // Тест:
 const s = new FakeAbortSignal();
-s.abort(); // скасування одразу
-promiseMapCancelable([1,2,3], num => Promise.resolve(num*2), { signal: s })
+setTimeout(() => s.abort(), 50);
+promiseMapCancelable([1,2,3], delayedDouble, { signal: s })
     .then(console.log)
-    .catch(err => console.error( err.message));
+    .catch(err => console.error(err.message));
